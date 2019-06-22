0fb38a8b242f231603c6e9087246f17204a3e740
MJ <pingukim@naver.com>
2019-06-22 14:50:44 +0900

    Timer 추가

--- src/components/First.js
+++ src/components/First.js
@@ -1,10 +1,48 @@
 import React, { Component } from 'react'
 import {CmCanvas} from "./CmCanvas";
 
+const INITIAL_TIME = 5;
+
 export class First extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isOn : false,
+            time : INITIAL_TIME
+        };
+        this.startTimer = this.startTimer.bind(this);
+        this.stopTimer = this.stopTimer.bind(this);
+    }
+
+    startTimer() {
+        if(this.state.isOn) return;
+
+        this.setState({
+            time: INITIAL_TIME,
+            isOn: true
+        });
+        this.timer = setInterval(() => {
+            if(this.state.time === 0) {
+                return this.stopTimer();
+            }
+            this.setState({
+                time: this.state.time - 1
+            })
+        }, 1000);
+    }
+    stopTimer() {
+        clearInterval(this.timer);
+        this.setState({isOn : false});
+    }
+
     render() {
         return (
-            <CmCanvas/>
+            <div>
+                <CmCanvas/>
+                <h3>timer: {this.state.time}</h3>
+                <button onClick={this.startTimer}>start</button>
+                <button onClick={this.stopTimer}>stop</button>
+            </div>
         )
     }
 }
