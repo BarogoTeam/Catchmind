2b70096ca7df871439ba2029f9cdde8cae18ad63
Jaeyeon Lee <jylee@spb.ai>
2019-06-24 21:55:22 +0900

    개발환경 살림

--- server/index.js
+++ server/index.js
@@ -6,22 +6,24 @@ const express = require('express')
 const app = express()
 const server = require('http').createServer(app)
 const portNo = 3001
+
 server.listen(portNo, () => {
   console.log('서버 실행 완료:', 'http://localhost:' + portNo)
 })
-// public 디렉터리를 공개합니다.  --- (※2)
-app.use('/public', express.static('./public'))
-app.get('/', (req, res) => { // 루트에 접근하면 /public로 리다이렉트
-  res.redirect(302, '/public')
-})
-// 웹 소켓 서버를 실행합니다. --- (※3)
+
+// index.html을 공개합니다.  --- (※2)
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+// // 웹 소켓 서버를 실행합니다. --- (※3)
 const socketio = require('socket.io')
 const io = socketio.listen(server)
 // 클라이언트가 접속했을 때의 이벤트 설정 --- (※4)
 
 io.on('connection', (socket) => {
   console.log('사용자 접속:', socket.client.id)
-  
+
   socket.on('join-room', (msg) => {
     console.log('join room', msg)
     socket.join(msg.roomid)
