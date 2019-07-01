7e32c2f31b20326c76fdde4a80fcff7433c0319e
Jaeyeon Lee <jylee@spb.ai>
2019-07-01 22:34:15 +0900

    First use of css module

--- src/components/Room/RoomContainer.js
+++ src/components/Room/RoomContainer.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
+import _ from 'lodash';
 import SocketContext from '../Context/SocketContext';
 import RoomContext from '../Context/RoomContext';
+
+import styles from './RoomContainer.module.css'
 
 export class RoomContainer extends Component {
     constructor() {
@@ -14,7 +17,11 @@ export class RoomContainer extends Component {
         return (
             <SocketContext.SocketProvider>
                 <RoomContext.RoomProvider roomId={roomId}>
-                    console.log 찍어놓음!
+                    <div
+                        className={_.values(styles).join(' ')}
+                    >
+                        console.log 찍어놓음!
+                    </div>
                 </RoomContext.RoomProvider>
             </SocketContext.SocketProvider>
         )
